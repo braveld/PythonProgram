37db60f20b72491396b32f47c2b291006d80841f
braveld <braveld@163.com>
2016-10-13 22:46:40 +0800

    sdfs

--- rongliang_prediction/data_discovery.py
+++ rongliang_prediction/data_discovery.py
@@ -3,13 +3,13 @@
 import pandas as pd
 from dateutil.parser import parse
 
-datafile = u'/home/bigdata/Downloads/data/data/测试容量变更记录明细数据.csv' #需要进行标准化的数据文件；
-final_data = pd.read_csv(datafile,encoding='utf8')
+datafile = u'F:/data/测试容量变更记录明细数据.csv' #需要进行标准化的数据文件；
+final_data = pd.read_csv(datafile,encoding='gbk')
 if __name__ == '__main__':
     final_data[u'距上一次暂停时长'] = -final_data[u'距上一次暂停时长']
     final_data[u'距上一次暂停恢复时长'] = -final_data[u'距上一次暂停恢复时长']
     final_data[u'距上一次减容恢复时长'] = -final_data[u'距上一次减容恢复时长']
     final_data[u'距上一次减容时长'] = -final_data[u'距上一次减容时长']
-    print final_data
-    # final_data.to_csv(datafile,encoding='gbk')
+
+    final_data.to_csv(datafile,encoding='gbk')
 
